d1aba14487b82c8d3dda22bb26a5474832ec1ba1
Rissa Rambe <71384953+rissarambe@users.noreply.github.com>
2021-09-17 18:40:35 +0700

    Add files via upload

---
+++ data/titik_destinasi_keretaapi_2.js
@@ -0,0 +1,12 @@
+var json_titik_destinasi_keretaapi_2 = {
+"type": "FeatureCollection",
+"name": "titik_destinasi_keretaapi_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Stasiun Tawang", "photo": "Stasiun Tawang_1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.4280338, -6.9645587 ] } },
+{ "type": "Feature", "properties": { "Name": "Tekodeko Koffiehuis", "photo": "Tekodeko_2.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.429488527854261, -6.967794842097377 ] } },
+{ "type": "Feature", "properties": { "Name": "Lawang Sewu", "photo": "Lawang Sewu_1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.410783099818104, -6.984007445834204 ] } },
+{ "type": "Feature", "properties": { "Name": "Museum Kereta Api Ambarawa, Jalan Stasiun, Panjang Kidul, Panjang, Semarang, Central Java", "photo": "Museum Ambarawa_1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.4044564, -7.2644736 ] } },
+{ "type": "Feature", "properties": { "Name": "Serabi Ngampin", "photo": "Serabi Ngampin_1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.387920036731799, -7.263565253010919 ] } }
+]
+}
---
+++ data/titik_fasilitas_keretaapi_3.js
@@ -0,0 +1,29 @@
+var json_titik_fasilitas_keretaapi_3 = {
+"type": "FeatureCollection",
+"name": "titik_fasilitas_keretaapi_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "RS Panti Wilasa Dr Cipto" }, "geometry": { "type": "Point", "coordinates": [ 110.4344181, -6.9730229 ] } },
+{ "type": "Feature", "properties": { "Name": "RS Bhakti Wira Tamtama" }, "geometry": { "type": "Point", "coordinates": [ 110.4078898, -6.986937 ] } },
+{ "type": "Feature", "properties": { "Name": "RS Hermina Pandanaran" }, "geometry": { "type": "Point", "coordinates": [ 110.4127684, -6.9855157 ] } },
+{ "type": "Feature", "properties": { "Name": "RSUD dr. Gunawan Mangunkusumo" }, "geometry": { "type": "Point", "coordinates": [ 110.4082718, -7.2678032 ] } },
+{ "type": "Feature", "properties": { "Name": "Bina Kasih" }, "geometry": { "type": "Point", "coordinates": [ 110.4011667, -7.2622222 ] } },
+{ "type": "Feature", "properties": { "Name": "Polsek Semarang Utara" }, "geometry": { "type": "Point", "coordinates": [ 110.429585, -6.968089 ] } },
+{ "type": "Feature", "properties": { "Name": "Polsek Semarang Barat" }, "geometry": { "type": "Point", "coordinates": [ 110.388498, -6.982372 ] } },
+{ "type": "Feature", "properties": { "Name": "Polsek Ambarawa" }, "geometry": { "type": "Point", "coordinates": [ 110.402891, -7.260833 ] } },
+{ "type": "Feature", "properties": { "Name": "SPBU Pertamina Pengapon" }, "geometry": { "type": "Point", "coordinates": [ 110.433053, -6.9628703 ] } },
+{ "type": "Feature", "properties": { "Name": "SPBU Pertamina" }, "geometry": { "type": "Point", "coordinates": [ 110.430588, -6.968922 ] } },
+{ "type": "Feature", "properties": { "Name": "SPBU 44.506.10" }, "geometry": { "type": "Point", "coordinates": [ 110.395919, -7.271566 ] } },
+{ "type": "Feature", "properties": { "Name": "SPBU Ngampin 44.507.16" }, "geometry": { "type": "Point", "coordinates": [ 110.3833293, -7.2663518 ] } },
+{ "type": "Feature", "properties": { "Name": "GPIB Immanuel Semarang (Gereja Blenduk)" }, "geometry": { "type": "Point", "coordinates": [ 110.4274767, -6.9683488 ] } },
+{ "type": "Feature", "properties": { "Name": "Masjid Istiqomah PT.KAI Tawang" }, "geometry": { "type": "Point", "coordinates": [ 110.4271758, -6.9651735 ] } },
+{ "type": "Feature", "properties": { "Name": "Gereja Katedral Santa Perawan Maria Ratu Rosario Suci" }, "geometry": { "type": "Point", "coordinates": [ 110.4098457, -6.9854797 ] } },
+{ "type": "Feature", "properties": { "Name": "Masjid Salafiyatul Huda" }, "geometry": { "type": "Point", "coordinates": [ 110.4113621, -6.9859546 ] } },
+{ "type": "Feature", "properties": { "Name": "Masjid Baitul Taqwa" }, "geometry": { "type": "Point", "coordinates": [ 110.400572, -7.263397 ] } },
+{ "type": "Feature", "properties": { "Name": "GKII Ambarawa" }, "geometry": { "type": "Point", "coordinates": [ 110.4025937, -7.2642383 ] } },
+{ "type": "Feature", "properties": { "Name": "Gereja Katolik Santo Yusuf Ambarawa" }, "geometry": { "type": "Point", "coordinates": [ 110.3972001, -7.2615294 ] } },
+{ "type": "Feature", "properties": { "Name": "Masjid Agung Palagan Ambarawa" }, "geometry": { "type": "Point", "coordinates": [ 110.3996526, -7.2606299 ] } },
+{ "type": "Feature", "properties": { "Name": "Kantong Parkir 1" }, "geometry": { "type": "Point", "coordinates": [ 110.428943, -6.96797 ] } },
+{ "type": "Feature", "properties": { "Name": "Kantong Parkir 2" }, "geometry": { "type": "Point", "coordinates": [ 110.426445066918831, -6.967604755193628 ] } }
+]
+}
